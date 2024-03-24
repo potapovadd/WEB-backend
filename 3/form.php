12abc5621169214e6f5b2fff1839a04707cfd4fe
potapovadd <144013043+potapovadd@users.noreply.github.com>
2024-03-24 15:30:56 +0300

    Add files via upload

---
+++ 3/form.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Форма</h1>
+
+    <form action="index.php"
+        method="POST">
+
+        <label>
+            ФИО: <br />
+            <input name="fio"
+            type="text"
+            placeholder="Введите ваше ФИО" />
+        </label> <br />
+
+        <label id = "telephone"> 
+            Номер телефона: <br />
+            <input name="tel"
+            type="tel"
+            placeholder="Введите ваш номер" />
+        </label> <br />
+        
+        <label>
+            Email: <br />
+            <input name="email"
+            type="email"
+            placeholder="Введите вашу почту" />
+        </label> <br />
+        
+        <label>
+            Дата рождения: <br />
+            <select name="year">
+            <?php 
+            for ($i = 1922; $i <= 2022; $i++) {
+            printf('<option value="%d">%d год</option>', $i, $i);
+            }
+            ?>
+            </select>
+
+            <select name="month">
+            <?php 
+            for ($i = 1; $i <= 12; $i++) {
+            printf('<option value="%d">%d месяц</option>', $i, $i);
+            }
+            ?>
+            </select>
+
+            <select name="day">
+            <?php 
+            for ($i = 1; $i <= 31; $i++) {
+            printf('<option value="%d">%d день</option>', $i, $i);
+            }
+            ?>
+            </select>
+        </label> <br />
+        
+        <label>
+            Пол: <br />
+            <input type="radio" checked="checked"
+            name="radio1" value="woman" />
+            Женский</label>
+        <label>
+            <input type="radio"
+            name="radio1" value="man" />
+            Мужской</label> <br />
+
+        <label>
+        Любимый язык программирования: <br />
+        <select name="lang[]"
+        multiple="multiple">
+        <option value="1">Pascal</option>
+        <option value="2">C</option>
+        <option value="3">C++</option>
+        <option value="4">JavaScript</option>
+        <option value="5">PHP</option>
+        <option value="6">Python</option>
+        <option value="7">Java</option>
+        <option value="8">Haskel</option>
+        <option value="9">Clojure</option>
+        <option value="10">Prolog</option>
+        <option value="11">Scala</option>
+        </select>
+        </label> <br />
+
+        <label>
+            Биография: <br />
+            <textarea name="bio">Я программист</textarea>
+        </label> <br />
+        
+        <label>
+            С контрактом ознакомлен(а): <br />
+            <input type="submit" value="Сохранить"
+            name="check-1" />
+        </label> <br />
+
+    </form>
+
+</body>
+</html>
---
+++ 3/index.php
@@ -0,0 +1,112 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  if (!empty($_GET['save'])) {
+    print('Спасибо, результаты сохранены.');
+  }
+  include('form.php');
+  exit();
+}
+
+$errors = FALSE;
+if (empty($_POST['fio']) || !preg_match('/^[а-яА-Яa-zA-Z\s]{1,150}$/', $_POST['fio'])) {
+  print('Заполните имя.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['tel']) || !preg_match('/^\+?([0-9]{11})/', $_POST['tel'])) {
+  print('Заполните телефон.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['email']) || !preg_match('/^[A-Za-z0-9_]+@[A-Za-z0-9_]+\.[A-Za-z0-9_]+$/', $_POST['email'])) {
+  print('Заполните почту.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
+  print('Заполните год.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['month']) || !is_numeric($_POST['month']) || !preg_match('/^\d+$/', $_POST['month'])) {
+  print('Заполните месяц.<br/>');
+  $errors = TRUE;
+}
+
+$months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+if (empty($_POST['day']) || $_POST['day'] > $months[$_POST['month'] - 1]) {
+  print('Заполните день.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['radio1'])) {
+  print('Заполните пол.<br/>');
+  $errors = TRUE;
+}
+
+$user = 'u67327'; 
+$pass = '4242183'; 
+$db = new PDO('mysql:host=localhost;dbname=u67327', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
+
+if (empty($_POST['lang'])) {
+  print('Заполните ЯП.<br/>');
+  $errors = TRUE;
+} else { 
+  $sth = $db->prepare("SELECT id FROM lang");
+  $sth->execute();
+
+  $langs = $sth->fetchAll();
+
+  foreach ($_POST['lang'] as $idlang) {
+    $errorlang = TRUE;
+    foreach ($langs as $lang) {
+      if ($idlang == $lang[0]) {
+        $errorlang = FALSE;
+        break;
+      }
+    }
+    if ($errorlang == TRUE) {
+      print('Заполните ЯП правильно.<br/>');
+      $errors = TRUE;
+      break;
+    }
+  }
+}
+
+if (empty($_POST['bio'])) {
+  print('Заполните био.<br/>');
+  $errors = TRUE;
+}
+
+if (empty($_POST['check-1']) || $_POST['check-1'] != 'on') {
+  print('Ознакомтесь.<br/>');
+  $errors = TRUE;
+}
+
+if ($errors) {
+  exit();
+}
+
+try {
+  $stmt = $db->prepare("INSERT INTO form SET fio = ?, tel = ?, email = ?, date = ?, gender = ?, bio = ?, check = ?");
+  $stmt->execute([$_POST['fio'], $_POST['tel'], $_POST['email'], $_POST['day'] . ':' . $_POST['month'] . ':' . $_POST['year'], $_POST['radio1'], $_POST['bio'], true]);
+
+  $id = $db->lastInsertId();
+
+  $stmt = $db->prepare("INSERT INTO form_lang (iduser, idlang) VALUES (:iduser, :idlang)");
+  foreach ($_POST['lang'] as $idlang) {
+    $stmt->bindParam(':iduser', $iduser);
+    $stmt->bindParam(':idlang', $idlang);
+    $iduser = $id;
+    $stmt->execute();
+  }
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
+
+header('Location: ?save=1');
