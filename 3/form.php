a49b00bbdf18fc7c57142b00d31e9f6a0d320943
potapovadd <144013043+potapovadd@users.noreply.github.com>
2024-03-24 16:01:04 +0300

    Add files via upload

--- 3/form.php
+++ 3/form.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>3</title>
 </head>
 <body>
     
